826b41378ed6588cbdf5abe874e122a77b5d1740
PhuocManh01 <huynhmanh999@gmail.com>
2026-09-21 16:26:11 +0700

    Add converter

--- src/converter.py
+++ src/converter.py
@@ -1,6 +1,6 @@
 def celsius_to_fahrenheit(celsius):
-    return (celsius * 9 / 5) + 32
+    return celsius * 9 / 5 + 32
 
-
-def km_to_miles(km):
-    return km * 0.621371
+""""""
+def fahrenheit_to_celsius(fahrenheit):
+    return (fahrenheit - 32) * 5 / 9
