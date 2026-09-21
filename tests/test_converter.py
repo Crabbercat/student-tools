f99b7bfd903769c6f37758188ceb1111321ded6e
PhuocManh01 <huynhmanh999@gmail.com>
2026-09-21 16:06:07 +0700

    Add temperature conversion

--- tests/test_converter.py
+++ tests/test_converter.py
@@ -1,9 +1,3 @@
-from src.converter import celsius_to_fahrenheit, km_to_miles
-
-
 def test_celsius_to_fahrenheit():
     assert celsius_to_fahrenheit(0) == 32
-
-
-def test_km_to_miles():
-    assert round(km_to_miles(1), 6) == 0.621371
+    assert celsius_to_fahrenheit(100) == 212
