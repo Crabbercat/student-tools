cce7413f6cb69d908b160df7d67955102c17a749
PhuocManh01 <huynhmanh999@gmail.com>
2026-09-21 16:26:46 +0700

    Add test_validator

--- tests/test_validator.py
+++ tests/test_validator.py
@@ -3,9 +3,7 @@ from src.validator import is_non_empty_text, is_positive_number
 
 def test_is_positive_number():
     assert is_positive_number(3)
-    assert not is_positive_number(-1)
 
 
 def test_is_non_empty_text():
     assert is_non_empty_text("hello")
-    assert not is_non_empty_text("   ")
