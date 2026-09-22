81b93b503d0ee3539027afc2be116c32b435ca4b
PhuocManh01 <huynhmanh999@gmail.com>
2026-09-22 20:41:10 +0700

    Add converter.py

--- src/converter.py
+++ src/converter.py
@@ -1,6 +1,11 @@
 def celsius_to_fahrenheit(celsius):
     return celsius * 9 / 5 + 32
 
+
+def kelvin_to_celsius(kelvin):
+    return kelvin - 273.15
+
+
 """Creates conflict with the test file, so I will comment it out for now."""
 # def fahrenheit_to_celsius(fahrenheit):
 #     return (fahrenheit - 32) * 5 / 9
